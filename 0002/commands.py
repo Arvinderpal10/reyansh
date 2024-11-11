f51c55eefc157d82ead09a1e3e24c92b55b97645
Arvinder Pal <arvinderpal10@gmail.com>
2024-11-11 08:23:06 +0530

    Day2 - Basic git commands

---
+++ 0002/commands.py
@@ -0,0 +1,14 @@
+'''
+git clone gitrepolink --- one time 
+
+git status    --- changed files
+
+git add . (all files) / filenames 
+git add Commands.py
+git add .
+
+git commit -m "Learned about basic commands"
+
+git push
+
+'''
